6502aa6843436dc67465099ee4b5bdeb1d0f621b
kubal <K8bus@proton.me>
2024-02-28 21:30:47 +0100

    Another Clean up

--- src/main/java/net/natureprairies/datagen/ModBlockTagProvider.java
+++ src/main/java/net/natureprairies/datagen/ModBlockTagProvider.java
@@ -2,6 +2,7 @@ package net.natureprairies.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.block.Blocks;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
 import net.natureprairies.block.Flowers;
@@ -19,110 +20,160 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
     protected void configure(RegistryWrapper.WrapperLookup arg) {
 
         getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                .add(Slate.DEEPSLATE_SLATE_ORE)
+
                 .add(Slate.SLATE_BLOCK)
                 .add(Slate.SLATE_BLOCK_SLAB)
                 .add(Slate.SLATE_BLOCK_STAIRS)
-                .add(Slate.DEEPSLATE_SLATE_ORE)
+                .add(Slate.SLATE_BLOCK_WALL)
+
                 .add(Slate.SMOOTH_SLATE)
                 .add(Slate.SMOOTH_SLATE_SLAB)
                 .add(Slate.SMOOTH_SLATE_STAIRS)
+                .add(Slate.SMOOTH_SLATE_WALL)
+
                 .add(Slate.POLISHED_SLATE)
                 .add(Slate.POLISHED_SLATE_SLAB)
                 .add(Slate.POLISHED_SLATE_STAIRS)
+                .add(Slate.POLISHED_SLATE_WALL)
+
                 .add(Slate.SLATE_BRICKS)
+                .add(Slate.CRACKED_SLATE_BRICKS)
                 .add(Slate.SLATE_BRICK_SLAB)
                 .add(Slate.SLATE_BRICK_STAIRS)
-                .add(Slate.CRACKED_SLATE_BRICKS)
+                .add(Slate.SLATE_BRICK_WALL)
+
                 .add(Slate.SMALL_SLATE_BRICKS)
+                .add(Slate.CRACKED_SMALL_SLATE_BRICKS)
                 .add(Slate.SMALL_SLATE_BRICK_SLAB)
                 .add(Slate.SMALL_SLATE_BRICK_STAIRS)
-                .add(Slate.CRACKED_SMALL_SLATE_BRICKS)
+                .add(Slate.SMALL_SLATE_BRICK_WALL)
+
                 .add(Slate.CHISELED_SLATE)
-                .add(Slate.SLATE_PILLAR)
-                .add(Slate.SLATE_LANTERN)
+
                 .add(Slate.SLATE_TILES)
+
+                .add(Slate.SLATE_PILLAR)
+
+                .add(Slate.SLATE_LANTERN)
+
                 .add(Slate.OVERGROWN_SLATE)
 
+
+                //.add(Blocks.QUARTZ_BLOCK)
+                //.add(Blocks.QUARTZ_STAIRS)
+                //.add(Blocks.QUARTZ_SLAB)
+                .add(Quartz.QUARTZ_BLOCK_WALL)
+
+                //.add(Blocks.SMOOTH_QUARTZ)
+                //.add(Blocks.SMOOTH_QUARTZ_STAIRS)
+                //.add(Blocks.SMOOTH_QUARTZ_SLAB)
+                .add(Quartz.SMOOTH_QUARTZ_WALL)
 
                 .add(Quartz.POLISHED_QUARTZ)
                 .add(Quartz.POLISHED_QUARTZ_SLAB)
                 .add(Quartz.POLISHED_QUARTZ_STAIRS)
+                .add(Quartz.POLISHED_QUARTZ_WALL)
 
+                //.add(Blocks.QUARTZ_BRICKS)
+                .add(Quartz.CRACKED_QUARTZ_BRICKS)
                 .add(Quartz.QUARTZ_BRICK_SLAB)
                 .add(Quartz.QUARTZ_BRICK_STAIRS)
-                .add(Quartz.CRACKED_QUARTZ_BRICKS)
+                .add(Quartz.QUARTZ_BRICK_WALL)
+
                 .add(Quartz.SMALL_QUARTZ_BRICKS)
+                .add(Quartz.CRACKED_SMALL_QUARTZ_BRICKS)
                 .add(Quartz.SMALL_QUARTZ_BRICK_SLAB)
                 .add(Quartz.SMALL_QUARTZ_BRICK_STAIRS)
-                .add(Quartz.CRACKED_SMALL_QUARTZ_BRICKS)
+                .add(Quartz.SMALL_QUARTZ_BRICK_WALL)
+
+                //.add(Blocks.CHISELED_QUARTZ_BLOCK)
+
                 .add(Quartz.QUARTZ_TILES)
+
+                //.add(Blocks.QUARTZ_PILLAR)
+
                 .add(Quartz.QUARTZ_LANTERN)
-                .add(Quartz.OVERGROWN_QUARTZ)
 
-                .add(Slate.SLATE_BLOCK_WALL)
-                .add(Quartz.QUARTZ_BLOCK_WALL)
-                .add(Slate.SMOOTH_SLATE_WALL)
-                .add(Quartz.SMOOTH_QUARTZ_WALL)
-                .add(Slate.POLISHED_SLATE_WALL)
-                .add(Quartz.POLISHED_QUARTZ_WALL)
-                .add(Slate.SLATE_BRICK_WALL)
-                .add(Quartz.QUARTZ_BRICK_WALL)
-                .add(Slate.SMALL_SLATE_BRICK_WALL)
-                .add(Quartz.SMALL_QUARTZ_BRICK_WALL);
-
+                .add(Quartz.OVERGROWN_QUARTZ);
 
         getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL)
+                .add(Slate.DEEPSLATE_SLATE_ORE)
+
                 .add(Slate.SLATE_BLOCK)
                 .add(Slate.SLATE_BLOCK_SLAB)
                 .add(Slate.SLATE_BLOCK_STAIRS)
-                .add(Slate.DEEPSLATE_SLATE_ORE)
+                .add(Slate.SLATE_BLOCK_WALL)
+
                 .add(Slate.SMOOTH_SLATE)
                 .add(Slate.SMOOTH_SLATE_SLAB)
                 .add(Slate.SMOOTH_SLATE_STAIRS)
+                .add(Slate.SMOOTH_SLATE_WALL)
+
                 .add(Slate.POLISHED_SLATE)
                 .add(Slate.POLISHED_SLATE_SLAB)
                 .add(Slate.POLISHED_SLATE_STAIRS)
+                .add(Slate.POLISHED_SLATE_WALL)
+
                 .add(Slate.SLATE_BRICKS)
+                .add(Slate.CRACKED_SLATE_BRICKS)
                 .add(Slate.SLATE_BRICK_SLAB)
                 .add(Slate.SLATE_BRICK_STAIRS)
-                .add(Slate.CRACKED_SLATE_BRICKS)
+                .add(Slate.SLATE_BRICK_WALL)
+
                 .add(Slate.SMALL_SLATE_BRICKS)
+                .add(Slate.CRACKED_SMALL_SLATE_BRICKS)
                 .add(Slate.SMALL_SLATE_BRICK_SLAB)
                 .add(Slate.SMALL_SLATE_BRICK_STAIRS)
-                .add(Slate.CRACKED_SMALL_SLATE_BRICKS)
+                .add(Slate.SMALL_SLATE_BRICK_WALL)
+
                 .add(Slate.CHISELED_SLATE)
-                .add(Slate.SLATE_PILLAR)
-                .add(Slate.SLATE_LANTERN)
+
                 .add(Slate.SLATE_TILES)
+
+                .add(Slate.SLATE_PILLAR)
+
+                .add(Slate.SLATE_LANTERN)
+
                 .add(Slate.OVERGROWN_SLATE)
 
 
+                //.add(Blocks.QUARTZ_BLOCK)
+                //.add(Blocks.QUARTZ_STAIRS)
+                //.add(Blocks.QUARTZ_SLAB)
+                .add(Quartz.QUARTZ_BLOCK_WALL)
+
+                //.add(Blocks.SMOOTH_QUARTZ)
+                //.add(Blocks.SMOOTH_QUARTZ_STAIRS)
+                //.add(Blocks.SMOOTH_QUARTZ_SLAB)
+                .add(Quartz.SMOOTH_QUARTZ_WALL)
 
                 .add(Quartz.POLISHED_QUARTZ)
                 .add(Quartz.POLISHED_QUARTZ_SLAB)
                 .add(Quartz.POLISHED_QUARTZ_STAIRS)
+                .add(Quartz.POLISHED_QUARTZ_WALL)
 
+                //.add(Blocks.QUARTZ_BRICKS)
+                .add(Quartz.CRACKED_QUARTZ_BRICKS)
                 .add(Quartz.QUARTZ_BRICK_SLAB)
                 .add(Quartz.QUARTZ_BRICK_STAIRS)
-                .add(Quartz.CRACKED_QUARTZ_BRICKS)
+                .add(Quartz.QUARTZ_BRICK_WALL)
+
                 .add(Quartz.SMALL_QUARTZ_BRICKS)
+                .add(Quartz.CRACKED_SMALL_QUARTZ_BRICKS)
                 .add(Quartz.SMALL_QUARTZ_BRICK_SLAB)
                 .add(Quartz.SMALL_QUARTZ_BRICK_STAIRS)
-                .add(Quartz.CRACKED_SMALL_QUARTZ_BRICKS)
-                .add(Quartz.QUARTZ_TILES)
-                .add(Quartz.QUARTZ_LANTERN)
-                .add(Quartz.OVERGROWN_QUARTZ)
+                .add(Quartz.SMALL_QUARTZ_BRICK_WALL)
 
-                .add(Slate.SLATE_BLOCK_WALL)
-                .add(Quartz.QUARTZ_BLOCK_WALL)
-                .add(Slate.SMOOTH_SLATE_WALL)
-                .add(Quartz.SMOOTH_QUARTZ_WALL)
-                .add(Slate.POLISHED_SLATE_WALL)
-                .add(Quartz.POLISHED_QUARTZ_WALL)
-                .add(Slate.SLATE_BRICK_WALL)
-                .add(Quartz.QUARTZ_BRICK_WALL)
-                .add(Slate.SMALL_SLATE_BRICK_WALL)
-                .add(Quartz.SMALL_QUARTZ_BRICK_WALL);
+                //.add(Blocks.CHISELED_QUARTZ_BLOCK)
+
+                .add(Quartz.QUARTZ_TILES)
+
+                //.add(Blocks.QUARTZ_PILLAR)
+
+                .add(Quartz.QUARTZ_LANTERN)
+
+                .add(Quartz.OVERGROWN_QUARTZ);
         getOrCreateTagBuilder(BlockTags.WALLS)
                 .add(Slate.SLATE_BLOCK_WALL)
                 .add(Quartz.QUARTZ_BLOCK_WALL)
